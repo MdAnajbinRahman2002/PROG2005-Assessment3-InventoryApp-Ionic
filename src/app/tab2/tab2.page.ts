960dfd3560a37af681921324d17e34580a2deb52
Ahmed Dewan <ahmeddewan1235@gmail.com>
2025-04-21 11:45:58 +1000

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
 
+//TAB 2 - Ionic Application
+// Group Work - Assessment#3 Group work
+//Adding items into data Set
+//typescript
+
 //all essentials imports
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -7,29 +12,34 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 //importing enum
+//directory to tab2.items.model.ts to import ENUM
 import { InventoryItem, Category, StockStatus } from '../models/tab2.item.model';
 
+//defining the classes
 @Component
 ({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule, CommonModule]
+  imports: [IonicModule, FormsModule, CommonModule] //these imports are required for ionic ui and form
 })
 
 export class Tab2Page 
 {
+  //this will show the success meesage
   showSuccessMessage = false; //show message
 
-
- //for dropdowns
+ //for dropdowns for enums
+ //dropdown can only be those whihc we declared
   categories = Object.values(Category);
   stockStatuses = Object.values(StockStatus);
 
-  allItems: InventoryItem[] = [];
-  featuredItems: InventoryItem[] = [];
+//this array will hold the items 
+  allItems: InventoryItem[] = [];       //all items
+  featuredItems: InventoryItem[] = [];  //feature items
 
+  //adding new items
   newItem: InventoryItem = 
   {
     itemName: '',
@@ -42,17 +52,22 @@ export class Tab2Page
     specialNote: ''
   };
 
+  //http client to call the APIs and Alert Controller
   constructor
   (
     private http: HttpClient,
     private alertController: AlertController
   ) {}
 
+  //repopulate and get all the items
   ngOnInit() 
   {
     this.loadItems();
   }
 
+  //this will add items to the server
+  //NOTE: the format whihc is used by SCU is not similiar with the coding we use
+  // we need to prepare data in the right formate
   addItem()
    {
     const postData = 
@@ -64,17 +79,19 @@ export class Tab2Page
       supplier_name: this.newItem.supplierName,
       stock_status: this.newItem.stockStatus,
       featured_item: this.newItem.featuredItem,
+      //optional
       special_note: this.newItem.specialNote || ''
     };
-  
+
+  //post request
     //pushing new item into the data server
+    //this system takes the input and pushes it into the server
     this.http.post('https://prog2005.it.scu.edu.au/ArtGalley', postData)
       .subscribe({
         next: () => 
         {
           this.showSuccessAlert('Item has been added successfully, Thanks');
           this.resetForm();
-
           this.loadItems(); //repopulate
         },
         error: (err) =>
@@ -93,7 +110,9 @@ export class Tab2Page
       .subscribe(
         {
         next: (items) => {
-          this.allItems = items.map(item => ({            //make sure we use the same case as used in dataset
+          //make sure we use the same case as used in dataset
+          //this will map and change the data into our invrntory structure
+          this.allItems = items.map(item => ({ 
             itemId: item.item_id,
             itemName: item.item_name,
             category: item.category,
@@ -128,7 +147,7 @@ export class Tab2Page
       specialNote: ''
     };
   }
-
+//this will show a success popup
   async showSuccessAlert(message: string) 
   {
     const alert = await this.alertController.create({
@@ -139,6 +158,7 @@ export class Tab2Page
     await alert.present();
   }
 
+  //error pop up if anything goes wrong
   async showErrorAlert(message: string) 
   {
     const alert = await this.alertController.create({
@@ -149,13 +169,18 @@ export class Tab2Page
     await alert.present();
   }
 
+  //help button at the end to the page
   async showHelp() {
     const alert = await this.alertController.create({
       header: 'Help TAB 2 - Add Items',
       message: `
-        ~ fill all the values to add an item into the 
+        To add a new item:
+        1. Fill in all the required fields (* marked).
+        2. Use dropdowns for Category and Stock Status.
+        3. Mark Featured if item is special.
+        4. Click 'Add Item' to submit. 
       `,
-      buttons: ['Understood'],
+      buttons: ['Right-ho'],
       cssClass: 'custom-alert'
     });
   
